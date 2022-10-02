8aaa378dfa618ae35cde72134dfbdf6bb878396c
Frawley <frawleyskid@gmail.com>
2022-10-02 03:00:50 -0400

    Add types defining the API for artifacts

---
+++ src/api.ts
@@ -0,0 +1,28 @@
+export type ArtifactFile = Readonly<{
+  name: string;
+  fileName: string;
+  mediaType?: string;
+  hash: string;
+  hashAlgorithm: string;
+  url: string;
+}>;
+
+export type ArtifactLink = Readonly<{
+  name: string;
+  link: string;
+}>;
+
+export type Artifact = Readonly<{
+  slug: string;
+  title: string;
+  summary: string;
+  description?: string;
+  files: ReadonlyArray<ArtifactFile>;
+  links: ReadonlyArray<ArtifactLink>;
+  people: ReadonlyArray<string>;
+  identities: ReadonlyArray<string>;
+  fromYear: number;
+  toYear?: number;
+  decades: ReadonlyArray<number>;
+  aliases: ReadonlyArray<string>;
+}>;
